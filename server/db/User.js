7a00b5fd212f4f9e36bb633a9d5da7e38a8db64f
Brandon Lowe <BLoweCLE@gmail.com>
2022-08-15 15:33:26 -0500

    over halfway finished

--- server/db/User.js
+++ server/db/User.js
@@ -1,12 +1,65 @@
-const Sequelize = require('sequelize');
+const {Sequelize, Op} = require('sequelize');
 const db = require('./db');
 
 const User = db.define('user', {
-  // Add your Sequelize fields here
-
+  name:{
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate:{
+      notEmpty: true
+    }
+  },
+  userType:{
+    type: Sequelize.STRING,
+    defaultValue: 'STUDENT',
+    allowNull: false,
+    validate:{
+      isIn: [['STUDENT', 'TEACHER']]
+    },
+  },
+  isStudent:{
+    type: Sequelize.VIRTUAL,
+    get(){
+      return `${this.userType}` === 'STUDENT' ? true : false;
+    },
+    set(value) {
+      throw new Error('Do not try to set the isStudent value!');
+    }
+  },
+  isTeacher:{
+    type: Sequelize.VIRTUAL,
+    get(){
+      return `${this.userType}` === 'TEACHER' ? true : false;
+    },
+    set(value) {
+      throw new Error('Do not try to set the isTeacher value!');
+    }
+  },
 });
 
+User.findUnassignedStudents = async function(){
+return await this.findAll(
+    {
+      where:{
+        mentorId:{
+          [Op.is]: null,
+        },
+        userType:{
+          [Op.in]: ['STUDENT'],
+        }
+    }
+  })
+}
 
+User.beforeUpdate(async (instance, options) => {
+  const mentor = await User.findOne({where:{id:instance.mentorId}});
+  console.log(mentor)
+  if((instance._previousDataValues.mentorId !== null && instance._previousDataValues.userType === 'STUDENT') || mentor.userType === 'STUDENT'){
+    throw new Error("Cannot update instance")
+  }
+
+})
 /**
  * We've created the association for you!
  *
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,63 @@ const {
   models: { User },
 } = require('../db');
 
+router.get('/unassigned', async (req, res, next) =>{
+  try{
+    const users = await User.findUnassignedStudents();
+    res.send(users)
+  }catch(err){
+    next(err);
+  }
+})
+
+router.get('/teachers', async (req, res, next) =>{
+  try{
+        res.send(await User.findTeachersAndMentees());
+  }catch(err){
+    next(err);
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try{
+    if(isNaN(parseInt(req.params.id, 10))){
+      res.sendStatus(400)
+    }
+    const user = await User.findByPk(req.params.id);
+    if(!user){
+      res.sendStatus(404);
+    } else {
+      await user.destroy();
+      res.sendStatus(204);
+    }
+  } catch(err){
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+      const newUser = await User.create(req.body);
+      res.status(201).send(newUser);
+  } catch (err) {
+    res.sendStatus(409);
+      next(err);
+  }
+});
+
+User.findTeachersAndMentees = async function(){
+    const teachers = await this.findAll({
+      where:{
+        userType: 'TEACHER'
+      },
+      include:{
+        model: User,
+        as: 'mentees',
+        foreignKey: 'mentorId',
+      }
+    });
+    return teachers;
+}
 /**
  * All of the routes in this are mounted on /api/users
  * For instance:
